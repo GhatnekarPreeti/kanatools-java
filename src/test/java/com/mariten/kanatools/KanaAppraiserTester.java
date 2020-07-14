a4928868e9423c1150e7ab7e165ce2ada203d4f9
GhatnekarPreeti <preeti.ghatnekar@embold.io>
2020-07-14 15:30:41 +0530

    Revert "test"
    This reverts commit 8cc8562d0cbf9b54f5ec85597bcc8cef0225b337.

--- src/test/java/com/mariten/kanatools/KanaAppraiserTester.java
+++ src/test/java/com/mariten/kanatools/KanaAppraiserTester.java
@@ -8,6 +8,5 @@ public abstract class KanaAppraiserTester
         // Test instantiation
         KanaAppraiser kana_evaluator_object = new KanaAppraiser();
 System.out.println("");
-System.out.println("");
     }
 }
